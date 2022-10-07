dc8ee3b53f27d5e331b3581838bedc588969aaa9
Emma ParisRosarioHA <rosarioherrera.a@gmail.com>
2022-10-07 20:24:58 -0300

    commit prueba

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,19 @@
-const cipher = {
-  // ...
+const cipher = {   // dentro de este elemento tenemos que implementar los medotos (una función dentro de otra) de cipher.encode(offset,string) y cipher.decode (offset, string)
+    function encode (encodeOffset, textForEncode){ //aqui dentro declaro las variables
+        const saludo = "hola";
+        const greeting = saludo + " " + textForEncode;
+        return greeting;
+        //console.log("aqui están", encodeOffset, textForEncode);
+    };
+
+
+
+
+    function decode (decodeOffset, textForDecode){ //aqui dentro declaro las variables
+        console.log (decodeOffset, textForDecode);
+    };
+
 };
 
-export default cipher;
+
+export default cipher; //ésta parte nos exporta todo hacia index.js
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import cipher from './cipher.js';
+import cipher from './cipher.js'; //ésta primera linea nos importa desde cipher.js
+
+//const inputEncode = document.getElementById("textForEncode");
+const encodeButton = document.getElementById("encodeButton");
+
+encodeButton.addEventListener ("click", function() {
+    const inputEncode = document.getElementById("textForEncode")
+
+
+    //const encodeText = inputEncode.value;
+    const encodedResult = cipher.encode(encodeText);
+    console.log(encodeText);
+    //document.getElementById("result").innerHTML = encodedResult;
+    alert(cipher.encode (encodedResult));
+    //cipher.encode(encodeText);
+});
+
+
 
 console.log(cipher);
