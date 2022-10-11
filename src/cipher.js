1ee40bf5e50657b87db20148e1697d94141b9d81
Emma ParisRosarioHA <rosarioherrera.a@gmail.com>
2022-10-10 22:34:51 -0300

    commit3

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,30 @@
 const cipher = {   // dentro de este elemento tenemos que implementar los medotos (una función dentro de otra) de cipher.encode(offset,string) y cipher.decode (offset, string)
-    encode : function (encodeOffset, textForEncode){ //aqui dentro declaro las variables
-        const saludo = "hola";
-        const greeting = saludo + " " + textForEncode;
-        return greeting;
-        //console.log("aqui están", encodeOffset, textForEncode);
+    encode: function (textForEncode, encodeOffset){ 
+        const letras = "ABC";
+        const resultadoEncode = letras + " " + textForEncode;
+        return resultadoEncode;
+        //console.log("ahora funciona ", textForEncode);
     },
+
+
+    decode: function (decodeOffset, textForDecode){
+       
+    },
+    
+    
+    // encode: function (encodeOffset,textForEncode){
+
+    // }
+    
+    
+    
+    
+    // encode : function (encodeOffset, textForEncode){ //aqui dentro declaro las variables
+    //     const saludo = "hola";
+    //     const greeting = saludo + " " + textForEncode;
+    //     return greeting;
+    //     //console.log("aqui están", encodeOffset, textForEncode);
+    // },
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 import cipher from './cipher.js'; //ésta primera linea nos importa desde cipher.js
 
-//const inputEncode = document.getElementById("textForEncode");
+const textForEncode = document.getElementById("textForEncode");
 const encodeButton = document.getElementById("encodeButton");
+//const encodeOffset = document.detElemetById("encodeOffset");
 
-encodeButton.addEventListener ("click", function() {
-    const inputEncode = document.getElementById("textForEncode")
-
-
-    const encodeText = inputEncode.value;
-    console.log(encodeText);
-    const encodedResult = cipher.encode(inputEncode);
-    console.log(inputEncode);
-    //document.getElementById("result").innerHTML = encodedResult;
-    alert(cipher.encode (encodedResult));
-    //cipher.encode(encodeText);
+encodeButton.addEventListener ("click",function(){
+    const inputEncode = textForEncode.value;
+    //const inputEncodeOffset = encodeOffset.value; 
+    alert(cipher.encode(inputEncode));
 });
 
 
+// const inputEncode = document.getElementById("textForEncode");
+// const encodeButton = document.getElementById("encodeButton");
 
-console.log(cipher);
+// encodeButton.addEventListener ("click", function() {
+//     const inputEncode = document.getElementById("textForEncode")
+
+
+//     const encodeText = inputEncode.value;
+//     console.log(encodeText);
+//     const encodedResult = cipher.encode(inputEncode);
+//     console.log(inputEncode);
+//     //document.getElementById("result").innerHTML = encodedResult;
+//     alert(cipher.encode (encodedResult));
+//     //cipher.encode(encodeText);
+// });
+
+
+
+//console.log(cipher);
