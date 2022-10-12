8eaf532e205d0280a466383c1d54465c4052fa1a
Emma ParisRosarioHA <rosarioherrera.a@gmail.com>
2022-10-12 15:23:01 -0300

    commit4

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,28 @@
 const cipher = {   // dentro de este elemento tenemos que implementar los medotos (una función dentro de otra) de cipher.encode(offset,string) y cipher.decode (offset, string)
-    encode: function (textForEncode, encodeOffset){ 
-        const letras = "ABC";
-        const resultadoEncode = letras + " " + textForEncode;
-        return resultadoEncode;
-        //console.log("ahora funciona ", textForEncode);
+    encode: function (encodeOffset, textForEncode){ 
+        // const encodeResult = "";
+        // if (encodeOffset === "" || textForEncode === "") throw new TypeError ("falta rellenar algunos parámetros")
+        // if (encodeOffset === null || textForEncode === null) throw new TypeError ("cuidado, estás intentando cifrar caracteres no permitidos")
+        // if (encodeOffset === 0 || textForEncode === 0) throw new TypeError ("necesitas elegir un valor diferente a Cero")
+        let encodeResult;
+        const letras = parseInt(encodeOffset);
+     for (let i=0; i< textForEncode.lenght; i++) { //calcula el largo del string y recorre cada letra con for
+        const x = textForEncode.charCodeAt(i);
+        if (x >= 65 && x <= 90) { //condicional que determina los minimos y maximos para la variable
+
+            encodeResult += textForEncode.fromCharCode (((x - 65 + letras) % 26) + 65); //formula cifrado cesar
+        }
+        else {
+             encodeResult += textForEncode.charAt (i); //permite reconocer espacios dentro del string
+         }
+     }   
+     return encodeResult;
+
     },
 
 
     decode: function (decodeOffset, textForDecode){
        
-    },
-    
-    
-    // encode: function (encodeOffset,textForEncode){
-
-    // }
-    
-    
-    
-    
-    // encode : function (encodeOffset, textForEncode){ //aqui dentro declaro las variables
-    //     const saludo = "hola";
-    //     const greeting = saludo + " " + textForEncode;
-    //     return greeting;
-    //     //console.log("aqui están", encodeOffset, textForEncode);
-    // },
-
-
-
-
-    /* decode : function (decodeOffset, textForDecode){ //aqui dentro declaro las variables
-        console.log(decodeOffset,textForDecode);
-    }, */
-
-};
-
-
+    }
+}
 export default cipher; //ésta parte nos exporta todo hacia index.js
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import cipher from './cipher.js'; //ésta primera linea nos importa desde cipher.js
-
 const textForEncode = document.getElementById("textForEncode");
 const encodeButton = document.getElementById("encodeButton");
-//const encodeOffset = document.detElemetById("encodeOffset");
+const encodeOffset = document.detElemetById("encodeOffset");
 
-encodeButton.addEventListener ("click",function(){
-    const inputEncode = textForEncode.value;
-    //const inputEncodeOffset = encodeOffset.value; 
-    alert(cipher.encode(inputEncode));
-});
+//transformar todo el string en mayúsculas
+textForEncode.addEventListener("keyup", function (){
+    this.value = this.value.toUpperCase ();
+}, true);
+
+
+
+
+encodeButton.addEventListener ("click",function(){ //al hacer click al botón
+    const textForEncodeValue = textForEncode.value; //traer el input de texto reutilizando la funcion textForEncode
+    const encodeOffsetValue = encodeOffset.value; // traer el numero de offset de desplazamiento, se diferencia al ser los valores del input
+    //const letras = parseInt(encodeOffset); // transforma de string a number el argumento
+    document.getElementById("inputEncode").value = cipher.encode(encodeOffsetValue, textForEncodeValue);
+
+
+
+    //const inputEncode = textForEncode.value;
+    //const inputEncodeOffset = encodeOffset.value;
+
+    //alert(cipher.encode(inputEncode));
+}, true);
 
 
 // const inputEncode = document.getElementById("textForEncode");
